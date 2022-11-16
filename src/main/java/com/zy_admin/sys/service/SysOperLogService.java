f80062972e73b4fcfdde8ee012f859b15f9ba72e
lvweilin <18382187601@163.com>
2022-11-16 09:12:12 +0800

    c

--- src/main/java/com/zy_admin/sys/service/SysOperLogService.java
+++ src/main/java/com/zy_admin/sys/service/SysOperLogService.java
@@ -1,7 +1,9 @@
 package com.zy_admin.sys.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.zy_admin.sys.entity.SysOperLog;
+import com.zy_admin.util.Result;
 
 /**
  * 操作日志记录(SysOperLog)表服务接口
@@ -11,5 +13,13 @@ import com.zy_admin.sys.entity.SysOperLog;
  */
 public interface SysOperLogService extends IService<SysOperLog> {
 
+    /**
+     * 分页查询所有数据
+     *
+     * @param page       分页对象
+     * @param sysOperLog 查询实体
+     * @return 所有数据
+     */
+    Result getOperLogs(Page page, SysOperLog sysOperLog,String startTime,String endTime);
 }
 
