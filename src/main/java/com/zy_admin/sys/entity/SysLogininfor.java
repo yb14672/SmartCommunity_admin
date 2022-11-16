e868f8658685194f3d96d160bca38eb0ee3b10c2
qu-yuxing <1664961187@qq.com>
2022-11-16 20:31:10 +0800

    导入验证正则验证重复验证为空1

--- src/main/java/com/zy_admin/sys/entity/SysLogininfor.java
+++ src/main/java/com/zy_admin/sys/entity/SysLogininfor.java
@@ -1,10 +1,10 @@
 package com.zy_admin.sys.entity;
 
-import java.util.Date;
-
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 系统访问记录(SysLogininfor)表实体类
@@ -15,22 +15,39 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class SysLogininfor extends Model<SysLogininfor> {
     //访问ID
+    @ExcelProperty("访问ID")
     private Long infoId;
+
     //用户账号
+    @ExcelProperty("用户账号")
     private String userName;
+
     //登录IP地址
+    @ExcelProperty("登录IP地址")
     private String ipaddr;
+
     //登录地点
+    @ExcelProperty("登录地点")
     private String loginLocation;
+
     //浏览器类型
+    @ExcelProperty("浏览器类型")
     private String browser;
+
     //操作系统
+    @ExcelProperty("操作系统")
     private String os;
+
     //登录状态（0成功 1失败）
+    @ExcelProperty("登录状态")
     private String status;
+
     //提示消息
+    @ExcelProperty("提示消息")
     private String msg;
+
     //访问时间
+    @ExcelProperty("访问时间")
     private Date loginTime;
 
 
