4bf041609c98c86bdcd36329187f4fcaaf07b259
yb14672 <yb14672@gmail.com>
2022-11-21 17:08:19 +0800

    房间只差下拉框

--- src/main/java/com/zy_admin/community/dto/BuildUnitDto.java
+++ src/main/java/com/zy_admin/community/dto/BuildUnitDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,5 +17,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BuildUnitDto extends ZyBuilding {
-    private List<ZyUnit> children;
+    /**
+     * 对应的单元
+     */
+    private List<ZyUnit> children = new ArrayList<ZyUnit>();
 }
