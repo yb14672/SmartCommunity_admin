499f95e80edf1a3df234f2a702eb9b1b7794a197
yb14672 <yb14672@gmail.com>
2022-11-22 09:58:29 +0800

    房间下拉已优化

--- src/main/java/com/zy_admin/community/dto/BuildAndUnit.java
+++ src/main/java/com/zy_admin/community/dto/BuildAndUnit.java
@@ -22,29 +22,13 @@ public class BuildAndUnit {
 
     public List<BuildUnitDto> build() {
         List<BuildUnitDto> buildUnitDtoList = new ArrayList<>();
-        if(buildingList.size()==1){
-            buildUnitDtoList.add(new BuildUnitDto(buildingList.get(0).getBuildingName(), buildingList.get(0).getBuildingId()));
-            if(unitList.size()==1){
-                if (buildUnitDtoList.get(0).getValue().equals(unitList.get(0).getBuildingId())){
-                    buildUnitDtoList.get(0).getChildren().add(new BuildUnitDto(unitList.get(0).getUnitName(),unitList.get(0).getUnitId()));
-                }
-            }else{
-                for (int i = 0; i < unitList.size() - 1; i++) {
-                    ZyUnit zyUnit = unitList.get(i);
-                    if (buildUnitDtoList.get(0).getValue().equals(zyUnit.getBuildingId())) {
-                        buildUnitDtoList.get(0).getChildren().add(new BuildUnitDto(zyUnit.getUnitName(), zyUnit.getUnitId()));
-                    }
-                }
-            }
-        }else{
-            for (int i = 0; i < buildingList.size() - 1; i++) {
-                ZyBuilding zyBuilding = buildingList.get(i);
-                buildUnitDtoList.add(new BuildUnitDto(zyBuilding.getBuildingName(), zyBuilding.getBuildingId()));
-                for (int j = 0; j < unitList.size() - 1; j++) {
-                    ZyUnit zyUnit = unitList.get(j);
-                    if (zyBuilding.getBuildingId().equals(zyUnit.getBuildingId())) {
-                        buildUnitDtoList.get(i).getChildren().add(new BuildUnitDto(zyUnit.getUnitName(), zyUnit.getUnitId()));
-                    }
+        for (int i = 0; i < buildingList.size(); i++) {
+            ZyBuilding zyBuilding = buildingList.get(i);
+            buildUnitDtoList.add(new BuildUnitDto(zyBuilding.getBuildingName(), zyBuilding.getBuildingId()));
+            for (int j = 0; j < unitList.size(); j++) {
+                ZyUnit zyUnit = unitList.get(j);
+                if (zyBuilding.getBuildingId().equals(zyUnit.getBuildingId())) {
+                    buildUnitDtoList.get(i).getChildren().add(new BuildUnitDto(zyUnit.getUnitName(), zyUnit.getUnitId()));
                 }
             }
         }
